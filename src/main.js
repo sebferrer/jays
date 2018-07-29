ab5fddd01e47e40a2875b8d75c59d635d5ad88fc
sebferrer <sebastien@sebferrer.fr>
2018-07-29 03:56:53 +0200

    Map generator

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ var canvas  = document.querySelector('#canvas');
 var canvas_W = 640;
 var canvas_H = 480;
 var ctx = canvas.getContext('2d');
+var bank = new ImgBank();
+var renderer = new Renderer();
 var gameState = new GameState(new Map(0));
 
 window.onload = function() {
@@ -11,6 +13,6 @@ window.onload = function() {
 function main() {
     //let jays = new Jays(50, 100);
     //let blob = new Blob(50, 50);
-    bank.preload(list, update);
-    update();
+    bank.preload(renderer);
+    renderer.update();
 }
--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,33 @@
-window.requestAnimFrame = (function() {
+class Renderer {
+    constructor() {}
+
+    render_map(map) {
+        for(let i = 0; i < map.height; i++) {
+            for(let j = 0; j < map.width; j++) {
+                let tile = map.tiles[i][j];
+                ctx.drawImage(bank.pic["assets/img/tiles.png"],
+                              tile.coord_y*tile.height, tile.coord_x*tile.width, tile.height, tile.width,
+                              j*tile.width, i*tile.height, tile.width, tile.height);
+            }
+        }
+    }
+    
+    update() {
+        ctx.save();
+        ctx.clearRect(0, 0, canvas_W, canvas_H);
+    
+        try {
+            this.render_map(gameState.current_map);
+        } catch (err) {}
+        
+        ctx.restore();
+        
+        var self = this;
+        window.requestAnimationFrame(function() { self.update() });
+    }
+}
+
+window.requestAnimationFrame = (function() {
     return window.requestAnimationFrame       ||
            window.webkitRequestAnimationFrame ||
            window.mozRequestAnimationFrame    ||
@@ -8,23 +37,3 @@ window.requestAnimFrame = (function() {
                window.setTimeout(callback, 1000 / 60);
            };
 })();
-
-function render_map(map) {
-    for(let i = 0; i < map.height; i++) {
-        for(let j = 0; j < map.width; j++) {
-            let tile = map.tiles[i][j];
-            ctx.drawImage(bank.pic["assets/img/tiles.png"], tile.coord_y*tile.height, tile.coord_x*tile.width, tile.height, tile.width, j*tile.width, i*tile.height, tile.width, tile.height);
-        }
-    }
-}
-
-function update() {
-	ctx.save();
-    ctx.clearRect(0, 0, canvas_W, canvas_H);
-
-    render_map(gameState.current_map);
-	
-	ctx.restore();
-	
-	window.requestAnimFrame(function() { update() });
-}
