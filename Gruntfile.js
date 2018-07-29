8b39513648f3a53b971156a3d80e72056ff852bf
Bastien Guillon <bastien.guillon42@gmail.com>
2018-07-30 00:59:07 +0200

    [add] prod build scripts (doesn't work yet because of ES6)

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.initConfig({
+		/* CONCAT */
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: ['src/**/**.js'],
+				dest: 'dist/main.js',
+			}
+		},
+		/* UGLIFY */
+		uglify: {
+			prod: {
+				options: {
+					mangle: true
+				},
+				files: {
+					'dist/main.js': ['dist/main.js']
+				}
+			}
+		},
+		/* COPY */
+		copy: {
+			prod: {
+				files: [
+					/* Index */
+					{
+						cwd: '.',
+						src: 'index.html',
+						dest: 'dist/index.html'
+					},
+					/* Css */
+					{
+						cwd: '.',
+						flatten: false,
+						src: 'css/**/*',
+						dest: 'dist/'
+					},
+					/* Assets */
+					{
+						cwd: '.',
+						flatten: false,
+						src: 'assets/**/*',
+						dest: 'dist/'
+					}
+				]
+			}
+		}
+	});
+
+	grunt.registerTask("debug", [
+		"concat",
+		"uglify:prod",
+		"copy:prod"
+	]);
+
+	grunt.registerTask("default", ["debug"]);
+};
