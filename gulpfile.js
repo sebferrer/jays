9efb5aad4357246f7fef49e36f4958a1a87041ba
Bastien Guillon <bastien.guillon42@gmail.com>
2018-08-07 23:57:56 +0200

    add clean, copy-css and copy-assets gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,45 @@
 var gulp = require("gulp");
+var del = require("del");
 var browserify = require("browserify");
-var source = require('vinyl-source-stream');
+var source = require("vinyl-source-stream");
 var tsify = require("tsify");
-var paths = {
-	pages: ['src/*.html']
-};
 
+/* Clean */
+gulp.task("clean", function () {
+	del("./dist");
+});
+
+/* Copy tasks */
 gulp.task("copy-html", function () {
-	return gulp.src(paths.pages)
+	return gulp.src("src/*.html")
 		.pipe(gulp.dest("dist"));
+});
+gulp.task("copy-css", function () {
+	return gulp.src("./css/**/*.css")
+		.pipe(gulp.dest("./dist/css"));
+});
+gulp.task("copy-assets", function () {
+	return gulp.src("./assets/**/*.*")
+		.pipe(gulp.dest("./dist/assets"));
 });
 
-gulp.task("default", ["copy-html"], function () {
-	return browserify({
-		basedir: '.',
-		debug: true,
-		entries: ['src/main.ts'],
-		cache: {},
-		packageCache: {}
-	})
-		.plugin(tsify)
-		.bundle()
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest("dist"));
-});
+gulp.task("default",
+	[
+		"clean",
+		"copy-html",
+		"copy-css",
+		"copy-assets"
+	],
+	function () {
+		return browserify({
+			basedir: ".",
+			debug: true,
+			entries: ["src/main.ts"],
+			cache: {},
+			packageCache: {}
+		})
+			.plugin(tsify)
+			.bundle()
+			.pipe(source("bundle.js"))
+			.pipe(gulp.dest("dist"));
+	});
