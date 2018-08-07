e976359eae15aecf81b41e8cd548fd7acc668085
Bastien Guillon <bastien.guillon42@gmail.com>
2018-08-08 00:25:14 +0200

    more viable build

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,39 @@
-var gulp = require("gulp");
-var del = require("del");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var tsify = require("tsify");
+var gulp = require("gulp"),
+	runSequence = require("run-sequence"),
+	del = require("del"),
+	browserify = require("browserify"),
+	source = require("vinyl-source-stream"),
+	tsify = require("tsify");
 
 /* Clean */
-gulp.task("clean", function () {
-	del("./dist");
-});
+gulp.task("clean", () => del(["./dist"]));
 
 /* Copy tasks */
-gulp.task("copy-html", function () {
-	return gulp.src("src/*.html")
+gulp.task("copy-html", () => gulp.src("src/*.html").pipe(gulp.dest("dist")));
+gulp.task("copy-css", () => gulp.src("./css/**/*.css").pipe(gulp.dest("./dist/css")));
+gulp.task("copy-assets", () => gulp.src("./assets/**/*.*").pipe(gulp.dest("./dist/assets")));
+gulp.task("copy-things", ["copy-html", "copy-css", "copy-assets"]);
+
+/* TS */
+gulp.task("ts", function () {
+	return browserify({
+		basedir: ".",
+		debug: true,
+		entries: ["src/main.ts"],
+		cache: {},
+		packageCache: {}
+	})
+		.plugin(tsify)
+		.bundle()
+		.pipe(source("bundle.js"))
 		.pipe(gulp.dest("dist"));
 });
-gulp.task("copy-css", function () {
-	return gulp.src("./css/**/*.css")
-		.pipe(gulp.dest("./dist/css"));
-});
-gulp.task("copy-assets", function () {
-	return gulp.src("./assets/**/*.*")
-		.pipe(gulp.dest("./dist/assets"));
-});
 
-gulp.task("default",
-	[
+/* Default */
+gulp.task("default", function () {
+	return runSequence(
 		"clean",
-		"copy-html",
-		"copy-css",
-		"copy-assets"
-	],
-	function () {
-		return browserify({
-			basedir: ".",
-			debug: true,
-			entries: ["src/main.ts"],
-			cache: {},
-			packageCache: {}
-		})
-			.plugin(tsify)
-			.bundle()
-			.pipe(source("bundle.js"))
-			.pipe(gulp.dest("dist"));
-	});
+		"copy-things",
+		"ts"
+	);
+});
