be955a38a59f01bd7b18cf67c34542f66e7dde61
Bastien Guillon <bguillon@lucca.fr>
2018-09-27 14:03:47 +0200

    make minimap room icons work + WIP update walls sprite

--- src/environment/floor_map.ts
+++ src/environment/floor_map.ts
@@ -9,7 +9,7 @@ import { DeadEndRightRoom } from "./rooms/dead_end_right_room";
 import { renderer, IMAGE_BANK } from "../main";
 import { IMiniMapColorConfig, IMiniMapConfiguration, IMiniMapSizeConfig } from "./iminimap_configuration";
 import { BossRoom } from "./rooms/boss_room";
-import { IIConRoom, isIIConRoom } from "./iicon_room";
+import { ICustomRoom, isCustomRoom } from "./iicon_room";
 
 const MINIMAP_CONFIG: IMiniMapConfiguration = {
 	colors: <IMiniMapColorConfig>{
@@ -23,8 +23,8 @@ const MINIMAP_CONFIG: IMiniMapConfiguration = {
 		active_fill: "#ffffff"
 	},
 	sizes: <IMiniMapSizeConfig>{
-		room_width: 26,
-		room_height: 26,
+		room_width: 30,
+		room_height: 30,
 		room_margin: 4
 	}
 };
@@ -64,45 +64,85 @@ export class FloorMap implements IDrawable {
 
 		for (let x = 0; x < this.maps_grid.length; ++x) {
 			for (let y = 0; y < this.maps_grid[x].length; ++y) {
-				const current = this.maps_grid[y][x];
-				if (current == null) {
+				const room = this.maps_grid[y][x];
+				if (room == null) {
 					continue;
 				}
 
-				if (current === this.current_room) {
-					context.strokeStyle = config.colors.active_border;
-					context.fillStyle = config.colors.active_fill;
-					context.lineWidth = 2;
-				} else if (current.has_been_visited) {
-					context.strokeStyle = config.colors.visited_border;
-					context.fillStyle = config.colors.visited_fill;
-					context.lineWidth = 1;
+				const position = new Point(x, y);
+				if (isCustomRoom(room)) {
+					this.draw_custom_room(context, room, config, position);
 				} else {
-					context.strokeStyle = config.colors.not_visited_border;
-					context.fillStyle = config.colors.not_visited_fill;
-					context.lineWidth = 1;
+					this.draw_standard_room(context, room, config, position);
 				}
-
-				const destination = new Point(
-					(config.sizes.room_width + config.sizes.room_margin) * x + config.sizes.room_margin,
-					(config.sizes.room_height + config.sizes.room_margin) * y + config.sizes.room_margin,
-				);
-
-				renderer.fill_round_rect(context, destination.x, destination.y, config.sizes.room_width, config.sizes.room_height, 3);
-				renderer.stroke_round_rect(context, destination.x, destination.y, config.sizes.room_width, config.sizes.room_height, 3);
-
-				// if (isIIConRoom(current)) {
-				// 	const icon_room = current as IIConRoom;
-				// 	context.drawImage(
-				// 		IMAGE_BANK.pic[icon_room.icon.sprite_sheet_path],
-				// 		icon_room.icon.top_left.x, icon_room.icon.top_left.y,
-				// 		icon_room.icon.width, icon_room.icon.height,
-				// 		config.sizes.room_width / 2 - icon_room.icon.width / 2,
-				// 		config.sizes.room_height / 2 - icon_room.icon.width / 2,
-				// 		icon_room.icon.width, icon_room.icon.height
-				// 	);
-				// }
 			}
 		}
+	}
+
+	private draw_standard_room(
+		context: CanvasRenderingContext2D,
+		room: RoomMap,
+		config: IMiniMapConfiguration,
+		position: Point
+	) {
+		if (room === this.current_room) {
+			context.strokeStyle = config.colors.active_border;
+			context.fillStyle = config.colors.active_fill;
+			context.lineWidth = 2;
+		} else if (room.has_been_visited) {
+			context.strokeStyle = config.colors.visited_border;
+			context.fillStyle = config.colors.visited_fill;
+			context.lineWidth = 1;
+		} else {
+			context.strokeStyle = config.colors.not_visited_border;
+			context.fillStyle = config.colors.not_visited_fill;
+			context.lineWidth = 1;
+		}
+
+		const destination = new Point(
+			(config.sizes.room_width + config.sizes.room_margin) * position.x + config.sizes.room_margin,
+			(config.sizes.room_height + config.sizes.room_margin) * position.y + config.sizes.room_margin,
+		);
+
+		renderer.fill_round_rect(context, destination.x, destination.y, config.sizes.room_width, config.sizes.room_height, 3);
+		renderer.stroke_round_rect(context, destination.x, destination.y, config.sizes.room_width, config.sizes.room_height, 3);
+	}
+
+	private draw_custom_room(
+		context: CanvasRenderingContext2D,
+		room: RoomMap & ICustomRoom,
+		base_config: IMiniMapConfiguration,
+		position: Point
+	) {
+		const destination = new Point(
+			(base_config.sizes.room_width + base_config.sizes.room_margin) * position.x + base_config.sizes.room_margin,
+			(base_config.sizes.room_height + base_config.sizes.room_margin) * position.y + base_config.sizes.room_margin,
+		);
+
+		const config = this.merge_color_config(base_config, room.color_configuration);
+
+		this.draw_standard_room(context, room, config, position);
+
+		const icon_room = room as ICustomRoom;
+		context.drawImage(
+			IMAGE_BANK.pictures[icon_room.icon.sprite_sheet_path],
+			icon_room.icon.top_left.x, icon_room.icon.top_left.y,
+			icon_room.icon.width, icon_room.icon.height,
+			destination.x + (base_config.sizes.room_width - icon_room.icon.width) / 2,
+			destination.y + (base_config.sizes.room_height - icon_room.icon.height) / 2,
+			icon_room.icon.width, icon_room.icon.height
+		);
+	}
+
+	private merge_color_config(
+		base_config: IMiniMapConfiguration,
+		color_configuration: IMiniMapColorConfig
+	): IMiniMapConfiguration {
+		const result = <IMiniMapConfiguration>{ sizes: {}, colors: {} };
+		Object.keys(base_config.sizes).forEach(object_key => result.sizes[object_key] = base_config.sizes[object_key]);
+		Object.keys(base_config.colors).forEach(key => {
+			result.colors[key] = color_configuration[key] != null ? color_configuration[key] : base_config.colors[key];
+		});
+		return result;
 	}
 }
--- src/environment/iicon_room.ts
+++ src/environment/iicon_room.ts
@@ -2,11 +2,11 @@ import { IMiniMapColorConfig } from "./iminimap_configuration";
 import { WallSprite } from "./walls/wall_sprite";
 
 /** Identifies a room which has a custom icon on the minimap */
-export interface IIConRoom {
+export interface ICustomRoom {
 	color_configuration: IMiniMapColorConfig;
 	icon: WallSprite;
 }
 
-export function isIIConRoom(object: any): object is IIConRoom {
+export function isCustomRoom(object: any): object is ICustomRoom {
 	return "color_configuration" in object && "icon" in object;
 }
